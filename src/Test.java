22055744d4aa3c4819b842f0dde72197eddf32b0
babz <babz@babz-laptop.(none)>
2010-11-10 02:56:06 +0100

    testfaelle

--- src/Test.java
+++ src/Test.java
@@ -26,7 +26,17 @@ public class Test{
 	Pretty b5 = new MorePretty();
 	String t6 = b5.transform("class A{ B c; ;D f { c.f(d)  {  /*x*/;{ /*y*/} }");
 	System.out.println("MorePretty:" + "\n" + t6);
-	}
 	
 	//VarPretty
+	Pretty b6 = new VarPretty(true); //longPretty
+	String t7 = b6.transform("class A{ B c; ;D f { c.f(d)  {  /*x*/;{ /*y*/} }");
+	System.out.println("VarPretty(long, without depth):" + "\n" + t7);
+	Pretty b8 = new VarPretty(true, 2); //longPretty
+	String t9 = b8.transform("class A{ B c; ;D f { c.f(d)  {  /*x*/;{ /*y*/} }");
+	System.out.println("VarPretty(long, with depth):" + "\n" + t9);
+	Pretty b7 = new VarPretty(false); //longPretty
+	String t8 = b7.transform("class A{ B c; ;D f { c.f(d)  {  /*x*/;{ /*y*/} }");
+	System.out.println("VarPretty(short):" + "\n" + t8);
+	
+	}
 }
