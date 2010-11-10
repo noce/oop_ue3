381d6edfe2fb8892caec5da25b4b8a3eaed4ddec
babz <babz@babz-laptop.(none)>
2010-11-10 02:45:57 +0100

    testfaelle

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,8 @@ public class Test{
 	Pretty b1 = new LongPretty(2);
 	String t1 = b1.transform("public class NoPretty extends BlockPretty{public NoPretty(){super(0);}}");
 	String t2 = b1.transform("class A{ B c; ;D f { c.f(d) /*x*/; /*y*/} }");
-	System.out.println(t1);
-	System.out.println(t2);
+	System.out.println("LongPretty:" + "\n" + t1);
+	System.out.println("LongPretty:" + "\n" + t2);
 	
 	//ShortPretty
 	Pretty b2 = new ShortPretty(2);
@@ -14,17 +14,19 @@ public class Test{
 	Pretty b3 = new ShortPretty(4);
 	String t4 = b3.transform("class A{ static main{ /*this is an exemple*/ x.z();    /*abc*/; if(f){g  ; if(h){ku}}" +
 			"				 else { ch; }     }}   ");
-	System.out.println(t3);
-	System.out.println(t4);
+	System.out.println("ShortPretty:" + "\n" + t3);
+	System.out.println("ShortPretty:" + "\n" + t4);
 	
 	//NoPretty
 	Pretty b4 = new NoPretty();
 	String t5 = b4.transform("class A{ B c; ;D f { c.f(d) /*x*/; /*y*/} }");
-	System.out.println(t5);
+	System.out.println("NoPretty:" + "\n" + t5);
 	
 	//MorePretty
-	Pretty b5 = new NoPretty();
-	String t6 = b5.transform("class A{ B c; ;D f { c.f(d) /*x*/; /*y*/} }");
-	System.out.println(t6);
+	Pretty b5 = new MorePretty();
+	String t6 = b5.transform("class A{ B c; ;D f { c.f(d)  {  /*x*/;{ /*y*/} }");
+	System.out.println("MorePretty:" + "\n" + t6);
 	}
+	
+	//VarPretty
 }
